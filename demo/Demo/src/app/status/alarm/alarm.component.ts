fbc259a3203417cf58c56e0c96d332b3074512a5
srikanth <snarala@ruggedmonitoring.com>
2024-05-19 11:08:23 +0530

    databinding

--- demo/Demo/src/app/status/alarm/alarm.component.ts
+++ demo/Demo/src/app/status/alarm/alarm.component.ts
@@ -1,18 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-alarm',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './alarm.component.html',
-  // styleUrl: './alarm.component.css'
-  styles:`
-  h4{
-    color:red;
-  }
-  `
+  styleUrl: './alarm.component.css'
+  // styles:`
+  // h4{
+  //   color:red;
+  // }
+  // `
 })
 export class AlarmComponent {
   id:number=9;
+  username:any='';
+  validusername:boolean=true;
+  updatingusername(event:Event){
+    this.username=(<HTMLInputElement>event.target).value;
+    // this.username='';
+    this.validusername=false;
+  }
 
 }
