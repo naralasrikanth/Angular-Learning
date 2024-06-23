5c220908f05ef8ff41722a92a0388d143f0491c0
srikanth <snarala@ruggedmonitoring.com>
2024-06-23 11:25:36 +0530

    ReactiveForms

--- demo/Demo/src/app/status/warning/warning.component.ts
+++ demo/Demo/src/app/status/warning/warning.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CustomValidators } from '../../custom-validators';
 
 @Component({
   selector: 'app-warning',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule,CommonModule,ReactiveFormsModule],
   templateUrl: './warning.component.html',
   styleUrl: './warning.component.css'
 })
@@ -16,11 +17,26 @@ export class WarningComponent {
   Age:null
 
  };
+ statusOfProject=['stable','critical','Finished']
  submitted= false;
+ projectData!:FormGroup;
+
+ngOnInit(){
+ this.projectData = new FormGroup({
+    'project':new FormControl('',[Validators.required,CustomValidators.invalidProjectName]),
+    'email': new FormControl('',[Validators.required,Validators.email]),
+    'projectStatus': new FormControl('Critical')
+  })
+}
+
   onSubmit(form: { valid: any; value: { name: string; email: string; Age: null; }; }){
     if(form.valid){
       this.data=form.value;
       this.submitted=true;
     }
+  }
+  onSave(){
+    console.log(this.projectData.value);
+    
   }
 }
